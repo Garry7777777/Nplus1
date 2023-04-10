3777ee2c2b002e62425df33516956a477c6db63d
Garry7777777 <garry4978@gmail.com>
2023-04-10 03:48:57 +0300

    init

---
+++ src/main/java/com/skypro/Nplus1/Nplus1App.java
@@ -0,0 +1,13 @@
+package com.skypro.Nplus1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nplus1App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nplus1App.class, args);
+	}
+
+}
---
+++ src/test/java/com/skypro/Nplus1/Nplus1AppTests.java
@@ -0,0 +1,12 @@
+package com.skypro.Nplus1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nplus1AppTests {
+	@Test
+	void contextLoads() {
+	}
+
+}
